38ae1ea45e925f90ab396df56d3da8f4aad55600
Luis Álvarez <luigi>
2025-08-12 13:41:41 -0400

    cambios en el hero

--- src/sections/HeroSection.jsx
+++ src/sections/HeroSection.jsx
@@ -8,7 +8,7 @@ export default function HeroSection() {
           <div style={{clipPath: "polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)"}} className="relative left-[calc(50%-11rem)] aspect-1155/678 w-144.5 -translate-x-1/2 rotate-30 bg-linear-to-tr from-[#5d8d99] to-[#9addff] opacity-30 sm:left-[calc(50%-30rem)] sm:w-288.75"></div>
         </div>
         
-        <div className="mx-auto max-w-2xl py-32 sm:py-25 lg:py-45">
+        <div className="mx-auto max-w-2xl py-20 sm:py-25 lg:py-30">
           <div className="hidden sm:mb-8 sm:flex sm:justify-center">
             <div className="relative rounded-full px-3 py-1 text-sm/6 text-gray-400 ring-1 ring-white/10 hover:ring-white/20">
               Nuestra historia e inicios <a href="#" className="font-semibold text-yellow-400"><span aria-hidden="true" className="absolute inset-0"></span>Leer más <span aria-hidden="true">&rarr;</span></a>
