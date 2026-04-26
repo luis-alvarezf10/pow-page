b066c6177016b27d6e878ed4afc56590a2c519c3
Luis Álvarez <luis.alvarez.developer@gmail.com>
2026-04-26 14:26:38 -0400

    particles updated

--- src/sections/ServicesSection.jsx
+++ src/sections/ServicesSection.jsx
@@ -86,7 +86,6 @@ export default function ServicesSection() {
           particleSpread={10}
           speed={0.1}
           particleBaseSize={100}
-          moveParticlesOnHover={true}
           alphaParticles={false}
           disableRotation={false}
         />
