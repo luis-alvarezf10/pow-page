9c8e508836c08e2f5e88054baa467395af053520
Luis Álvarez <luis.alvarez.developer@gmail.com>
2026-04-26 14:25:40 -0400

    particles updated

--- src/sections/PricingSection.jsx
+++ src/sections/PricingSection.jsx
@@ -54,7 +54,6 @@ export default function PricingSection() {
           particleSpread={10}
           speed={0.1}
           particleBaseSize={100}
-          moveParticlesOnHover={true}
           alphaParticles={false}
           disableRotation={false}
         />
